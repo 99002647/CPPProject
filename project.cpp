2fb56cf7a7c35dd7a7c86ef47bb89f36de4b9e96
99002647 <pradeep.v@ltts.com>
2020-10-21 22:06:43 +0530

    Initial Commit

---
+++ project.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    public:
+    int rno;
+    char name[30];
+    float mark;
+    
+        void readData()
+        {
+            cout << "Enter the Roll No: ";
+            cin >> rno;
+            cout << "Enter the Name: ";
+            cin >> name;
+            cout << "Enter the Total Mark: ";
+            cin >> mark;
+        }
+        void writeData()
+        {
+            cout <<"\n===============\nStudent Details\n===============\n";
+            cout << "Roll No: "<< rno << endl;
+            cout << "Name: "<< name << endl;
+            cout << "Mark: "<< mark << endl;
+        }
+};
+
+int main()
+{
+    Student s1;
+    s1.readData();
+    s1.writeData();
+}
